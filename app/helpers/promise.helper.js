c3b4b6af58b69fabaf78b2e4acba824b12826a46
lqkiet <lequockiet1512@gmail.com>
2022-03-04 23:02:06 +0700

    Cai dat cac handler truy cap CSDL

--- app/helpers/promise.helper.js
+++ app/helpers/promise.helper.js
@@ -1,7 +1,7 @@
 const handlePromise = (promise) => {
     return promise
         .then((data) => [nul, data])
-        .catch((error) => [error, underfined]);
+        .catch((error) => [error, undefined]);
 };
 
 module.exports = handlePromise;
--- app/routes/contact.routes.js
+++ app/routes/contact.routes.js
@@ -3,27 +3,39 @@ const contacts = require("../controllers/contact.controller");
 
 module.exports = (app) => {
     const router = express.Router();
-    router.get("/", contacts.findAll);
-    router.post("/", contacts.create);
-    router.delete("/", contacts.deleteAll);
-    router.get("/favorite", contacts.findAllFavorite);
-    router.get("/:id", contacts.findOne);
-    router.put("/:id", contacts.update);
-    router.delete("/", contacts.delete);
+
+    router.route("/")
+        .get(contacts.findAll)
+        .post(contacts.create)
+        .delete(contacts.deleteAll);
+        
+    router.route("/favorite")
+        .get(contacts.findAllFavorite);
+
+    router.route("/:id")
+        .get(contacts.findOne)
+        .put(contacts.update)
+        .delete(contacts.delete);
+    // Create a new contact
+    // router.post("/",contacts.create);
+
+    // //Retrieve all contacts
+    // router.get("/", contacts.findAll);
+
+    // //Retrieve all favorite contacts
+    // router.get("/favorite", contacts.findAllFavorite);
+
+    // //Retrieve a single contact with id
+    // router.get("/:id", contacts.findOne);
+    
+    // //Update a contact with id
+    // router.put("/:id", contacts.update);
+
+    // //Delete a contact with id
+    // router.delete("/:id", contacts.delete);
+
+    // //Delete all contacts
+    // router.delete("/", contacts.deleteAll);
+
     app.use("/api/contacts", router);
 };
-//Cac route duoc to chuc gon
-// module.exports = (app) => {
-//     const router = express.Router();
-//     router.route("/")
-//         .get(contacts.findAll)
-//         .post("/", contacts.create)
-//         .delete("/", contacts.deleteAll);
-//     router.route("/favorite")
-//         .get(contacts.findAllFavorite);
-//     router.route("/:id")
-//         .get(contacts.findOne)
-//         .put(contacts.update)
-//         .delete(contacts.delete);
-//     app.use("/api/contacts", router);
-// };
